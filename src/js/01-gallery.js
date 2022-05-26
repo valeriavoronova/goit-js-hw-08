95d2413877c9b2f619345303ade16ec322412155
Valeria Voronova <66879713+valeriavoronova@users.noreply.github.com>
2022-05-26 14:57:50 +0100

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -22,41 +22,10 @@ const items = galleryItems.map(( { preview, original, description } ) => {
 
 galleryRef.insertAdjacentHTML("beforeend", items);
 
-//create event
-galleryRef.addEventListener("click", onImageClick);
 
 var lightbox = new SimpleLightbox('.gallery a', {captionsData: `alt`, captionDelay: 250});
-// import SimpleLightbox from "simplelightbox";
-// Описан в документации
+
 import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-function onImageClick(e){
-    if(e.target.nodeName !== 'IMG'){
-        return;
-    }
-
-      e.preventDefault();
-
-      const instance = basicLightbox.create(
-      `<img src="${e.target.dataset.source}" width="800" height="600">
-      `, {
-        onShow: () => window.addEventListener('keydown', onEscClose),
-        onClose: () => window.removeEventListener('keydown', onEscClose)
-      })
-
-      instance.show();
-
-      // if (basicLightbox.visible()){
-      //   window.addEventListener('keydown', onEscClose);
-      //   //console.log('повесила слушатель');
-      // }
-
-      function onEscClose(e){
-        //console.log(e);
-        if(e.code === 'Escape')
-        instance.close();
-
-      }
-}
